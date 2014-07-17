9b7d42fa837523d597585976a07a7d9f06781716
Marc Bernstein <github@marcanderica.org>
2014-07-17 14:09:02 -0700

    OmniAuth init

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+
+	def self.create_with_omniauth(auth)
+		create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+      user.user_name = auth["info"]["user_name"]
+			user.image_url = auth["extra"]["raw_info"]["image_url"]
+			user.small_image_url = auth["extra"]["raw_info"]["small_image_url"]
+			user.token = auth["credentials"]["token"]
+			user.token_secret = auth["credentials"]["secret"]
+			user.user_id = auth["extra"]["raw_info"]["id"]
+			user.link = auth["extra"]["raw_info"]["link"]
+    end
+	end
+
 end
